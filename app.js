c135170ae860446aec540bb4e228830fa99ccaa1
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-08-20 19:20:13 +0430

    name generator

---
+++ app.js
@@ -0,0 +1,42 @@
+document.querySelector("#generate-names").addEventListener("submit", loadNames);
+
+function loadNames(event) {
+     event.preventDefault();
+
+     const region = document.getElementById("country").value;
+     const gender = document.getElementById("genre").value;
+     const amount = document.getElementById("amount").value;
+
+     // make url
+     let url = "http://uninames.com/api/?";
+     if (region !== " ") url += `region = ${region}`;
+     if (gender !== " ") url += `gender = ${gender}`;
+     if (amount !== " ") url += `amount = ${amount}`;
+     console.log(url);
+
+     // ajax
+     const xhr = new XMLHttpRequest();
+
+     xhr.open("GET", url, true);
+
+     xhr.onload = function () {
+          if (this.status === 200) {
+               const names = JSON.parse(this.responseText);
+               //    console.log(names);
+
+               // insert into html
+               let html = "<h2>Generated Names: </h2>";
+               html += '<ul class="list" > ';
+               names.forEach(function (name) {
+                    html += `
+                <li> ${name.name} </li>
+                `;
+               });
+               html += "<ul/>";
+               document.querySelector("#result").innerHtml = html;
+          }
+     }; //end of xhr func
+
+     //  send
+     xhr.send();
+} // end of loadNames func
